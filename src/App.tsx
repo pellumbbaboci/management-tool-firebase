9743d5c4216637d1d044b4c477f72a5d88ed29d1
Pellumb Baboci <pellumbbaboci@gmail.com>
2021-09-22 12:45:29 +0200

    config of tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>Bla bla</header>
+      <header className='bg-black text-white'>Bla bla</header>
     </div>
   );
 }
